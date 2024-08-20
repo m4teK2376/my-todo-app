a6dd2a38e30bdd438ac720b1ef31c5fa5b04e0e5
Matija <radakovic.matija@gmail.com>
2024-08-20 18:18:22 +0200

    Implemented delete function when todo is finished

---
+++ web.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import functions
+
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] +"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My todo app")
+
+count = 0
+
+for todo in todos:
+    checkbox = st.checkbox(todo, key=count)
+    if checkbox:
+        todos.pop(count)
+        functions.write_todos(todos)
+        del st.session_state[count]
+        st.rerun()
+
+    count += 1
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.session_state

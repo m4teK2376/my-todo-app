18b420e90eb7bf8af7a77e3d07162b4d6852cbee
Matija <radakovic.matija@gmail.com>
2024-08-20 19:02:18 +0200

    Initial commit

--- web.py
+++ web.py
@@ -24,7 +24,7 @@ for todo in todos:
 
     count += 1
 
-st.text_input(label="", placeholder="Add new todo...",
+st.text_input(label=" ", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
 
 st.session_state
